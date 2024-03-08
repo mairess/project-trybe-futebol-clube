13aa6b307883a24e4ef3b12cc1fe0442f17084a9
mairess <trabalho.maires@gmail.com>
2024-03-08 16:37:38 -0300

    perf: remove unnecessary method signature

--- app/backend/src/Interfaces/matches/IMatchModel.ts
+++ app/backend/src/Interfaces/matches/IMatchModel.ts
@@ -2,7 +2,6 @@ import IMatch from './IMatch';
 import { ICRUDModelReader } from '../ICRUDModel';
 
 export interface IMatchModel extends ICRUDModelReader<IMatch> {
-  findAll(): Promise<IMatch[]>
   findAllMatchesInProgress(): Promise<IMatch[]>
   findAllMatchesFinished(): Promise<IMatch[]>
   findAllSimultaneous(id1: number, id2: number):Promise<IMatch[] | null>
