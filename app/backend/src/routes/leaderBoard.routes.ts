97a224e4052efe0dc957d5f56e6a72c50a85dd1e
mairess <trabalho.maires@gmail.com>
2024-03-08 08:27:47 -0300

    feat: route /matches implemented

---
+++ app/backend/src/routes/leaderBoard.routes.ts
@@ -0,0 +1,10 @@
+// import { Router } from 'express';
+// import LeaderBoardController from '../controllers/LeaderBoardController';
+
+// const leaderBoardController = new LeaderBoardController();
+
+// const router = Router();
+
+// router.get('/home', (req, res) => leaderBoardController.getRank(req, res));
+
+// export default router;
