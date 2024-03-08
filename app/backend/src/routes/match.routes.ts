44f10467bb18994f65fba1c47f947521c8805fba
mairess <trabalho.maires@gmail.com>
2024-03-08 09:12:24 -0300

    fix: route /matches

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -6,11 +6,9 @@ const matchController = new MatchController();
 
 const router = Router();
 
-router.get('/:id', Validations
-  .validateToken, (req: Request, res: Response) => matchController.getMatchById(req, res));
+router.get('/:id', (req: Request, res: Response) => matchController.getMatchById(req, res));
 
-router.get('/', Validations
-  .validateToken, (req: Request, res: Response) => matchController.getAllMatches(req, res));
+router.get('/', (req: Request, res: Response) => matchController.getAllMatches(req, res));
 
 router.patch('/:id', Validations
   .validateToken, (req: Request, res: Response) => matchController.updateMatch(req, res));
